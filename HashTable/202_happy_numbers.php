3aaf396a680e1df4ac5818a3e2d6c96138b7aa31
Khalid Hassan Sonchay <sonchay64@live.com>
2024-01-09 02:50:07 +0600

    LeetCode 202_happy_numbers using PHP

---
+++ HashTable/202_happy_numbers.php
@@ -0,0 +1,77 @@
+<?php 
+
+// Available @ https://leetcode.com/problems/happy-number/submissions/1140833277
+
+
+/*
+
+Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+
+ 
+
+Example 1:
+
+Input: n = 19
+Output: true
+Explanation:
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+Example 2:
+
+Input: n = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= n <= 231 - 1
+
+*/
+
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Boolean
+     */
+    function isHappy($n) {
+
+        $is_cycle = 0;
+
+        while ($n != 1 || $n == 1)
+        {
+            $num = str_split($n);
+
+             $pow_res=0;
+            
+            foreach($num as $numbers)
+            {
+            	$pow_res += pow(intval($numbers),2);
+            }
+            
+            $n=$pow_res;
+                
+            if($n == 1) {return true;}
+            else if($is_cycle > 50) {return false;}
+
+            $is_cycle++;
+        }
+        
+       
+    }
+}
+
+$sol = new Solution();
+$in=19;
+var_export($sol->isHappy($in));
+
+?>
